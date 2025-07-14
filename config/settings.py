4cc17e88dd9cbdf018e34f398d330e3a136ded15
umidasirov <126274140+umidasirov@users.noreply.github.com>
2025-07-14 14:47:25 +0500

    new

--- config/settings.py
+++ config/settings.py
@@ -16,8 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATIC_DIR = [os.path.join('/staticfiles')]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
